d195da9de33ec61fecec145911d53e4c0a588734
Lukus Klipping <lukusklipping@gmail.com>
2025-06-30 20:49:02 -0500

    Time: 0 ms (100%), Space: 14.9 MB (37.15%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        
-        for(int i = 0; i < n-1; i++){
-            for(int j = i+1; j < n;j++){
-                if(nums[i] + nums[j] == target){
-                    return {i, j};
-                }
+        unordered_map<int, int> map;
+
+        for(int i = 0; i < nums.size(); i++){
+            int comp = target - nums[i];
+
+            if(map.find(comp) != map.end())
+            {
+                return {map[comp], i};
             }
+            map[nums[i]] = i;
         }
         return {};
     }
